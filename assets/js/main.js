bd078bf3a43949014be2321228b989f3f917b450
Aritra Das <aritrarick2002@gmail.com>
2022-10-12 22:03:19 +0530

    Made some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,11 +83,13 @@ let swiper = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-        el: '.swiper-pagination',
+        el: ".swiper-pagination",
         clickable: true,
     },
+    mousewheel: true,
+    keyboard: true,
 });
